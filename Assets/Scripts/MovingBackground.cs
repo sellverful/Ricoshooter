56a18f8ca8d6717d1952bec8dd8631af0357950f
EmperorOfCatkind <kirillaleksandrov84@gmail.com>
2018-06-03 17:14:23 +0200

    Small changes

--- Assets/Scripts/MovingBackground.cs
+++ Assets/Scripts/MovingBackground.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class MovingBackground : MonoBehaviour {
     public Transform[] planes;
+    private int first;
+    private int last;
+    private Vector3 firstPosition;
+    private Vector3 lastPosition;
+
+    public float backgroundSize;
 	// Use this for initialization
 	void Start () {
         planes = new Transform[transform.childCount];
         for(int i = 0; i < transform.childCount; i++)
+        {
+            planes[i] = transform.GetChild(i);
+            first = 0;
+            last = planes.Length - 1;
+            firstPosition = transform.GetChild(first).transform.position;
+            lastPosition = transform.GetChild(last).transform.position;
+        }
 	}
   
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        for (int i = 0; i < transform.childCount; i++)
+        {
+
+        }
+    }
 }
