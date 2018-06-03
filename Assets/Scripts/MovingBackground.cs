baf400a8168eda53a9c67e1c0c0b432057ded9a0
EmperorOfCatkind <kirillaleksandrov84@gmail.com>
2018-06-03 16:14:46 +0200

    Second part of new level

---
+++ Assets/Scripts/MovingBackground.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBackground : MonoBehaviour {
+    public Transform[] planes;
+	// Use this for initialization
+	void Start () {
+        planes = new Transform[transform.childCount];
+        for(int i = 0; i < transform.childCount; i++)
+	}
+  
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
