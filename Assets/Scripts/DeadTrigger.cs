edf1aaa868b846b164673184b4e5cf8f672fbc48
EmperorOfCatkind <kirillaleksandrov84@gmail.com>
2018-06-17 18:09:36 +0200

    Enemies on train roof; you fall you ded

---
+++ Assets/Scripts/DeadTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadTrigger : MonoBehaviour {
+
+    public PlayerController player;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("im ded");
+
+            player.DealDamage(3);
+        }
+    }
+}
