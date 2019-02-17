5d8e5d13872ea6769b19528a5d5315825bc3cbbd
Viacheslav Shiliak <sellverful@gmail.com>
2019-02-17 16:39:47 +0100

    Added hacking game to first level

--- Assets/Scripts/ForPresentation/Binds.cs
+++ Assets/Scripts/ForPresentation/Binds.cs
@@ -4,35 +4,44 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class Binds : MonoBehaviour {
+public class Binds : MonoBehaviour
+{
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         DontDestroyOnLoad(gameObject);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyUp(KeyCode.Alpha2) && SceneManager.GetActiveScene().name != "FirstBoss")
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Alpha3) && SceneManager.GetActiveScene().name != "BossSecond")
         {
-            Load("FirstBoss");
-        } else if (Input.GetKeyUp(KeyCode.Alpha1) && SceneManager.GetActiveScene().name != "FirstLevel")
+            Load("BossSecond");
+        }
+        else if (Input.GetKeyUp(KeyCode.Alpha1) && SceneManager.GetActiveScene().name != "FirstLevel")
         {
             Load("FirstLevel");
         }
-	}
-    void Load (string scene)
+        else if (Input.GetKeyUp(KeyCode.Alpha2) && SceneManager.GetActiveScene().name != "FourthLevel_2")
+        {
+            Load("FourthLevel_2");
+        }
+    }
+    void Load(string scene)
     {
         GameObject loaderObject = GameObject.Find("Death/LevelLoader");
-        
+
         if (loaderObject != null)
         {
             LevelLoader loader = loaderObject.GetComponent<LevelLoader>();
             StartCoroutine(loader.LoadAsynchronously(scene));
-        } else
+        }
+        else
         {
             SceneManager.LoadScene(scene);
         }
-        
+
     }
 }
